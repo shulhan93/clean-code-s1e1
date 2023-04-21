0d7509698535f8a9fe1b5ed9bba86ce0b45f29ba
shulhan93 <shulhan.andrei@gmail.com>
2023-04-21 10:36:02 +0300

    refactor(basis-1.3): add double quotes

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
   label.innerText=taskString;
-  label.className='task';
+  label.className="task";
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
@@ -39,7 +39,7 @@ var createNewTaskElement=function(taskString){
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
   editButton.className="edit";
   deleteButton.className="delete";
-  deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.src="./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
   //and appending.
   listItem.appendChild(checkBox);
@@ -70,12 +70,12 @@ var addTask=function(){
 
 var editTask=function(){
   console.log("Edit Task...");
-  console.log("Change 'edit' to 'save'");
+  console.log("Change "edit" to "save"");
 
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector('input[type=text]');
+  var editInput=listItem.querySelector("input[type=text]");
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".edit");
   var containsClass=listItem.classList.contains("editMode");
@@ -181,7 +181,7 @@ for (var i=0; i<completedTasksHolder.children.length;i++){
 
 
 
-// Issues with usability don't get seen until they are in front of a human tester.
+// Issues with usability don"t get seen until they are in front of a human tester.
 
 //prevent creation of empty tasks.
 
